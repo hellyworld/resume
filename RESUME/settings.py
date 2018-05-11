f8b199f055191422f95b7a5c62b2b1381b7e4bbc
Adrian Helerea <helereaiadrian@gmail.com>
2018-05-11 07:35:07 +0300

    remove static_root media_root

--- RESUME/settings.py
+++ RESUME/settings.py
@@ -118,10 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static_root')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'media_root'),
